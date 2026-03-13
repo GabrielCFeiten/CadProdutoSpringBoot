835b664523989c00d55dc92595bd1162f355185d
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-12 21:10:47 -0300

    EstudanteModel criado

---
+++ src/main/java/com/cf/CadProduto/CadProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.cf.CadProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cf/CadProduto/CadProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.CadProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
