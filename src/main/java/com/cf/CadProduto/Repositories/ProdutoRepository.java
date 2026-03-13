4e29902c8ab4be5952307c05e0a5ef0d861ff9d3
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-12 21:14:10 -0300

    ProdutoRepository criado

--- src/main/java/com/cf/CadProduto/Repositories/ProdutoRepository.java
+++ src/main/java/com/cf/CadProduto/Repositories/ProdutoRepository.java
@@ -1,1 +1,8 @@
+package com.cf.CadProduto.Repositories;
 
+import com.cf.CadProduto.Model.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProdutoRepository extends JpaRepository<ProdutoModel,Long> {
+}
